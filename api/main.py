dd2959f262e8a3d106e211c2c65133a11cf8ba97
Adrien Lindeberg <adrien.lindeberg@gmail.com>
2024-10-08 20:06:25 +0200

    Initialisation projet

---
+++ api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+
+
+
+
+app = FastAPI()
+
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
+
+
+@app.get("/")
+async def hello():
+    return {"hello": "world"}
